b9ff284a63b54b092bbe07ee8df47792aa99be9b
Viktoriia0555 <viktoriiahrynchuk@gmail.com>
2023-03-02 10:22:00 +0200

    save

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const { Users } = require('./models/users');
+const { Links } = require('./models/links');
+
+console.log(`MONGO_DB_URI:${process.env.MONGO_DB_URI}`);
+
+const Mongo = require('./setup/mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+
+const setup = async () => {
+ await Mongo.setupDb(process.env.MONGO_DB_URI);
+
+ app.listen(process.env.PORT, () => {
+  console.log(`Server was started on ${process.env.PORT}`);
+ });
+};
+
+setup();
