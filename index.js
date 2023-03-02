d1fd498ae11c6acad7f5f4fd7a0314ac92ee834c
Viktoriia0555 <viktoriiahrynchuk@gmail.com>
2023-03-02 10:23:19 +0200

    save

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const { Links } = require('./models/links');
 
 console.log(`MONGO_DB_URI:${process.env.MONGO_DB_URI}`);
 
+
+
+
+
 const Mongo = require('./setup/mongoose');
 
 const app = express();
 app.use(bodyParser.json());
 
-const setup = async () => {
- await Mongo.setupDb(process.env.MONGO_DB_URI);
+const setup = async() => {
+    await Mongo.setupDb(process.env.MONGO_DB_URI);
 
- app.listen(process.env.PORT, () => {
-  console.log(`Server was started on ${process.env.PORT}`);
- });
+    app.listen(process.env.PORT, () => {
+        console.log(`Server was started on ${process.env.PORT}`);
+    });
 };
 
 setup();
